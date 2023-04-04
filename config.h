99aa96adbd46816aa3740048076f379cf3590d50
shasherazi <hassanrandomz@gmail.com>
2023-04-05 03:23:09 +0500

    bro who tf are you

--- config.h
+++ config.h
@@ -4,19 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono NF:size=12"
+	"JetBrains Mono NF:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	            /*     fg         bg       */
-	[SchemeNorm] = { "#d5c7ac", "#0d0000" },
-	[SchemeSel] = { "#26130d", "#869aa4" },
-	[SchemeOut] = { "#d5c7ac", "#0e3523" },
+	[SchemeNorm] = { "#a9b1d6", "#1a1b26" },
+	[SchemeSel] = { "#1a1b26", "#a9b1d6" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 32;
+static unsigned int lineheight = 39;
 static unsigned int min_lineheight = 8;
 
 /*
