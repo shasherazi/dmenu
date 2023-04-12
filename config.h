e52fd31fe20b723dc67df1c57a26a69d370ea5dc
shasherazi <hassanrandomz@gmail.com>
2023-04-13 02:22:09 +0500

    01100101 01101101 01110000 01110100 01111001 00100000 01100011 01101111 01101101 01101101 01101001 01110100 00100000 01101101 01100101 01110011 01110011 01100001 01100111 01100101

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 39;
+static unsigned int lineheight = 32;
 static unsigned int min_lineheight = 8;
 
 /*
